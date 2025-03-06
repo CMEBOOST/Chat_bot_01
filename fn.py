0839340334423da23ad9144a49f0c9251f235343
Surachet <surachet2421@gmail.com>
2025-03-06 15:55:16 +0700

    add some features

--- fn.py
+++ fn.py
@@ -1,7 +1,16 @@
 import time
+import streamlit as st
+from datetime import datetime
 
 
 def stream_data(info):
     for word in info.split(" "):
         yield word + " "
         time.sleep(0.10)
+
+def time_naja():
+    time_display = st.empty()
+    while True:
+        current_time = datetime.now().strftime("%H:%M:%S")
+        time_display.text(f"เวลาปัจจุบัน: {current_time}")
+        time.sleep(1)
--- views/about_me.py
+++ views/about_me.py
@@ -1,5 +1,10 @@
+import time
 import streamlit as st
-from fn import stream_data
+from fn import stream_data,time_naja
+from datetime import datetime
+
+
+
 @st.dialog('Contace Me')
 def show_contact_from():
     st.text_input("Frist Name")
@@ -9,7 +14,7 @@ col1, col2 = st.columns(2, gap="small", vertical_alignment="center")
 with col1:
     st.image("assets/pic.jpg", width=230, )
 with col2:
-    st.header('OOP - ChatBOT')
+    st.header('OOP - ChatBOT',divider="gray")
     st.write(
         "นาย สุรเชษฐ์ สีสา ระหัสนักศึกษา 67114540583"
     )
@@ -20,10 +25,13 @@ st.write("\n")
 info_01 = st.button("Information!")
 
 info__ = """
-เรียนอาจารย์ อาจารย์ ดร.วิชิต สมบัติ และ อาจารย์ ประจำวิชาทุกท่าน\n
-:material/settings: ใช้ AI Moel : DeepSeek-R1 เพราะเป็นตัวที่ดีตอบคำถามได้อย่างครบถ้วน และไม่กินพื้นที่ทรัพยากร")
+:material/settings: ใช้ AI Moel : DeepSeek-R1 เพราะเป็นตัวที่ดีตอบคำถามได้อย่างครบถ้วน และไม่กินพื้นที่ทรัพยากร
+*Function ที่มี*
+- asdsadasdasd
+- asdasdjsahdjahsdsa
+- asdmkasjdiasjdoijasodijas
 """
 
 if info_01:
+    st.markdown('**เรียนอาจารย์ อาจารย์ ดร.วิชิต สมบัติ และ อาจารย์ ประจำวิชาทุกท่าน**')
     st.write(stream_data(info__))
-
